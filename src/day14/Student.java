52e76ba13b4271323fea6501395f6b6cabc0b69a
rashika <rashika98@gmail.com>
2021-03-18 08:12:43 -0400

    day14 class 03/18/2021

---
+++ src/day14/Student.java
@@ -0,0 +1,27 @@
+package day14;
+
+public class Student {
+
+    String name;
+    int age;
+
+    public Student(String n, int a) {
+        name = n;
+        age = a;
+        System.out.println("constructor called....");
+    }
+
+    public void displayInfo() {
+        System.out.println("Name is: " + name);
+        System.out.println("age is: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("ram", 22);
+        student1.displayInfo();
+        System.out.println("==============");
+        Student student2 = new Student("shyam", 33);
+        student2.displayInfo();
+
+    }
+}
---
+++ src/day14/ThisDemo1.java
@@ -0,0 +1,30 @@
+package day14;
+
+public class ThisDemo1 {
+
+    //instance variable
+    int a = 5;
+
+    //non-static method
+    public void display() {
+        System.out.println("this is: " + this);
+        System.out.println(a);//this.a
+        System.out.println(this.a);
+        //local variable
+        int a = 6;
+        System.out.println(a);
+        System.out.println(this.a);
+    }
+
+    public static void main(String[] args) {
+        ThisDemo1 ob1 = new ThisDemo1();
+        System.out.println("ob1 is: " + ob1);
+        ob1.display();
+        System.out.println("======================");
+
+        ThisDemo1 ob2 = new ThisDemo1();
+        System.out.println("ob2 is: " + ob2);
+        ob2.display();
+
+    }
+}
